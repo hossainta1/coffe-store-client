1a45b64bef854d4e70d81f6e1b3bdec27d5c296d
hossainta1 <tanvir.hossain303030@gmail.com>
2023-11-08 22:02:35 +0600

    Create Add Coffe Component

---
+++ src/components/AddCoffe.jsx
@@ -0,0 +1,101 @@
+
+
+const AddCoffe = () => {
+    const handleAddCoffe = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const qyantity = form.qyantity.value;
+        const suppliar = form.suppliar.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffe = { name, qyantity, suppliar, taste, category, details, photo };
+        console.log(newCoffe);
+
+    }
+    return (
+        <div className="bg-[#F4F3F0] p-24">
+            <h2 className="text-3xl font-extrabold">Add Coffe</h2>
+
+            <form onSubmit={handleAddCoffe}>
+                {/* from row coffe */}
+                <div className="md:flex mb-8">
+                    <div className="form-control w-1/2">
+                        <label className="label">
+                            <span className="label-text">Coffe Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="coffe Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="qyantity" placeholder="Available Quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* from supliar row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control w-1/2">
+                        <label className="label">
+                            <span className="label-text">Suppliar Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="suppliar" placeholder="Suppliar Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Taste</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="taste" placeholder="Teast" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* from category and details */}
+                <div className="md:flex mb-8">
+                    <div className="form-control w-1/2">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="category" placeholder="Category" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="details" placeholder="Details" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* from photo Url row  */}
+                <div className="mb-8">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="photo" placeholder="photo url" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+
+                <input type="submit" value="Add Coffe" className="btn btn-block bg-black text-white" />
+            </form>
+        </div>
+
+
+    );
+};
+
+export default AddCoffe;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffe from './components/AddCoffe.jsx';
+import UpdateCoffe from './components/UpdateCoffe.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>
+  },
+  {
+    path: "addCoffe",
+    element: <AddCoffe></AddCoffe>
+  },
+  {
+    path: "updateCoffee",
+    element: <UpdateCoffe></UpdateCoffe>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
